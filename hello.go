7ff3de438513cb9511c309e1e1a7c975e4178838
hriships <hrishishinde.p@gmail.com>
2018-05-15 13:30:35 +0530

    added new feature
    - added lanague support for Spain, India

--- hello.go
+++ hello.go
@@ -3,16 +3,32 @@ package main
 import "fmt"
 
 const helloPrefix = "Hello, "
+const helloPrefixSpanish = "Hola, "
+const helloPrefixIndia = "Namaste, "
+
+const spanish = "Spanish"
+const india = "India"
 
 func Hello() string {
 	return helloPrefix + "world"
 }
 
-func HelloString(name string) string {
+func HelloString(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return helloPrefix + name
+
+	prefix := helloPrefix
+
+	if language == spanish {
+		prefix = helloPrefixSpanish
+	}
+
+	if language == india {
+		prefix = helloPrefixIndia
+	}
+
+	return prefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -20,15 +20,30 @@ func TestHelloWithArgument(t *testing.T) {
 	}
 
 	t.Run("Saying hello to folks", func(t *testing.T) {
-		got := HelloString("Hrishi")
+		got := HelloString("Hrishi", "")
 		want := "Hello, Hrishi"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say Hello World when empty string is suplied", func(t *testing.T) {
-		got := HelloString("")
+		got := HelloString("", "")
 		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In spanish", func(t *testing.T) {
+		got := HelloString("Chmouel", "Spanish")
+		want := "Hola, Chmouel"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("In India", func(t *testing.T) {
+		got := HelloString("Nabo", "India")
+		want := "Namaste, Nabo"
 
 		assertCorrectMessage(t, got, want)
 	})
