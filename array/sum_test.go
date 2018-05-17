7973335a23e2f798315c063795930272920662d0
hriships <hrishishinde.p@gmail.com>
2018-05-17 21:40:35 +0530

    refactored slices test case

--- array/sum_test.go
+++ array/sum_test.go
@@ -17,42 +17,41 @@ func TestSum(t *testing.T) {
 	})
 }
 
+func assertSlices(t *testing.T, got, want []int) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
 func TestSumAll(t *testing.T) {
 	t.Run("two collections", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{3, 4})
 		want := []int{3, 7}
 
-		if !reflect.DeepEqual(want, got) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertSlices(t, got, want)
 	})
 
 	t.Run("single collection", func(t *testing.T) {
 		got := SumAll([]int{1, 2, 3})
 		want := []int{6}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertSlices(t, got, want)
 	})
 }
 
 func TestSumTailAll(t *testing.T) {
+
 	t.Run("two collections", func(t *testing.T) {
 		got := SumTailAll([]int{1, 2}, []int{0, 4})
 		want := []int{2, 4}
 
-		if !reflect.DeepEqual(want, got) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertSlices(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumTailAll([]int{}, []int{2, 3})
 		want := []int{0, 3}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertSlices(t, got, want)
 	})
 }
