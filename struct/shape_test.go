e5e9e29e6d695af7a3d1590439e66cbe1d16d821
hriships <hrishishinde.p@gmail.com>
2018-05-22 09:11:54 +0530

    refactored test - added verbosity to tests

--- struct/shape_test.go
+++ struct/shape_test.go
@@ -18,20 +18,23 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10.0}, 314.1592653589793},
-		{Triangle{12.0, 6.0}, 36.0},
+		{name: "rectangle", shape: Rectangle{12.0, 6.0}, want: 72.0},
+		{name: "circle", shape: Circle{10.0}, want: 314.1592653589793},
+		{name: "triangle", shape: Triangle{12.0, 6.0}, want: 36.0},
 	}
 
 	for _, test := range areaTests {
-		got := test.shape.Area()
-		want := test.want
+		t.Run(test.name, func(t *testing.T) {
+			got := test.shape.Area()
+			want := test.want
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+			if got != want {
+				t.Errorf("%#v got %.2f want %.2f", test.shape, got, want)
+			}
+		})
 	}
 }
